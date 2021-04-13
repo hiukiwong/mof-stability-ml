621f8cfd5894febc7471014a5be76bf9790308eb
hiukiwong <hiukiwong@gmail.com>
2021-04-13 11:05:36 +0100

    Reflect new input path

--- config/path_handler.py
+++ config/path_handler.py
@@ -4,6 +4,6 @@ from pathlib import Path
 current_directory = Path(os.path.dirname(os.path.abspath(__file__)))
 project_dir = current_directory.parent
 
-input_files_directory = Path(os.path.join(project_dir, "Inputs"))
+input_files_directory = Path(os.path.join(project_dir, "RASPA_Output"))
 output_files_directory = Path(os.path.join(project_dir, "Outputs"))
 sambvca21_full_path = Path(os.path.join(project_dir, "Sambvca21_source//sambvca21.x"))
