e9f2524e536cd87f1aaf264dfe799c65c81fe076
hiukiwong <hiukiwong@gmail.com>
2021-04-18 23:03:53 +0100

    Abstract create cif function

--- pre-process.py
+++ pre-process.py
@@ -9,44 +9,50 @@ from pathlib import Path
 
 def write_raspa_input_file(simulation_type, number_of_cycles, print_every, framework, framework_name, unit_cells):
     molecule_name_list = []
-    f = open("simulation.input", "w+")
+    simulation_file_name = "simulation.input"
+    f = open(simulation_file_name, "w+")
     f.write("SimulationType     %s\n" % simulation_type)
     f.write("NumberofCycles     %d\n" % number_of_cycles)
     f.write("PrintEvery     %d\n\n" % print_every)
     f.write("Framework      %s\n" % framework)
     f.write("FrameworkName      %s\n" % framework_name)
     f.write("UnitCells      %g %g %g\n" % (unit_cells[0], unit_cells[1], unit_cells[2]))
+    f.close()
+    return simulation_file_name
     # return (molecule_name_list, unit_cells)
 
+def raspa_create_cif(mof_name):
+    shutil.rmtree("VTK")
+    shutil.rmtree("Restart")
+    shutil.rmtree("Output")
+    raspa_output_folder_path = "RASPA_Output/"
+    if not os.path.exists(raspa_output_folder_path):
+        os.makedirs(raspa_output_folder_path)
 
-write_raspa_input_file("MonteCarlo", 1, 1, 0, "IRMOF-1", [2, 2, 2])
+    cif_filename = "Framework_0_initial_2_2_2_P1.cif"
+    cif_src_path = os.path.join("Movies/System_0/", cif_filename)
 
-subprocess.run(["/home/hiuki/RASPA/src/simulate", "simulation.input"])
-shutil.rmtree("VTK")
-shutil.rmtree("Restart")
-shutil.rmtree("Output")
-raspa_output_folder_path = "RASPA_Output/"
-if not os.path.exists(raspa_output_folder_path):
-    os.makedirs(raspa_output_folder_path)
+    cif_dest_path = os.path.join(raspa_output_folder_path, f"{mof_name}.cif")
+    xyz_dest_path = os.path.join(raspa_output_folder_path, f"{mof_name}.xyz")
 
-cif_filename = "Framework_0_initial_2_2_2_P1.cif"
-cif_src_path = os.path.join("Movies/System_0/", cif_filename)
+    for filename in os.listdir("Movies/System_0"):
+        if filename.endswith("Framework_0_initial_2_2_2_P1.cif"):
+            shutil.copy(cif_src_path, cif_dest_path)
 
-def name_of_mof(filepath: str) -> str:
-    with open(filepath, "r") as f:
-        first_line = f.readline()
-        mof_name = first_line.split("data_", 1)[1].split("\n", 1)[0]
-        f.close()
-    return mof_name
+    shutil.rmtree("Movies")
+    return cif_dest_path, xyz_dest_path
 
-mof_name = name_of_mof(cif_src_path)
-cif_dest_path = os.path.join(raspa_output_folder_path, f"{mof_name}.cif")
-xyz_dest_path = os.path.join(raspa_output_folder_path, f"{mof_name}.xyz")
 
-for filename in os.listdir("Movies/System_0"):
-    if filename.endswith("Framework_0_initial_2_2_2_P1.cif"):
-        shutil.copy(cif_src_path, cif_dest_path)
-
-shutil.rmtree("Movies")
-
+mof_name = "IRMOF-1"
+sim_input_file_path = write_raspa_input_file(
+    simulation_type="MonteCarlo",
+    number_of_cycles=1,
+    print_every=1,
+    framework=0,
+    framework_name=mof_name,
+    unit_cells=[2, 2, 2]
+)
+path_to_raspa = "/home/hiuki/RASPA2/src/simulate"
+subprocess.run([path_to_raspa, sim_input_file_path])
+cif_dest_path, xyz_dest_path = raspa_create_cif(mof_name)
 subprocess.run(["obabel",  "-icif", cif_dest_path, "-oxyz", "-O", xyz_dest_path])
